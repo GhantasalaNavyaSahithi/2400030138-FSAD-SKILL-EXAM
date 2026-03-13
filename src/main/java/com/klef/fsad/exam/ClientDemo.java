e7e5eb8ca30d8b6df958e8f53900d91d884c10b6
GhantasalaNavyaSahithi <2400030138@kluniversity.in>
2026-03-13 09:11:30 +0530

    Removed AppTest.java and fixed project errors

--- src/main/java/com/klef/fsad/exam/ClientDemo.java
+++ src/main/java/com/klef/fsad/exam/ClientDemo.java
@@ -1,5 +1,50 @@
 package com.klef.fsad.exam;
 
+import org.hibernate.*;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import java.util.*;
+
 public class ClientDemo {
 
+    public static void main(String[] args) {
+
+        Configuration cfg = new Configuration();
+        cfg.configure("hibernate.cfg.xml");
+        cfg.addAnnotatedClass(Transport.class);
+
+        SessionFactory sf = cfg.buildSessionFactory();
+        Session session = sf.openSession();
+
+        Transaction tx = session.beginTransaction();
+
+        Transport t = new Transport();
+        t.setName("Bus");
+        t.setDate(new Date());
+        t.setStatus("Available");
+
+        session.persist(t);
+
+        tx.commit();
+
+        System.out.println("Record Inserted");
+
+        Transaction tx2 = session.beginTransaction();
+
+        String hql = "from Transport";
+        Query<Transport> query = session.createQuery(hql, Transport.class);
+
+        List<Transport> list = query.getResultList();
+
+        for(Transport tr : list)
+        {
+            System.out.println(tr.getId()+" "+tr.getName()+" "+tr.getStatus());
+        }
+
+        tx2.commit();
+
+        session.close();
+        sf.close();
+    }
 }
